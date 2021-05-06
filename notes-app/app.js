e7bc66a75c1b87c41ce77e8c1d42a54a936c0aa7
Misyani-DS <54836758+Misyani-DS@users.noreply.github.com>
2021-05-06 18:13:37 +0800

    Importing Node.js Core Modules

---
+++ notes-app/app.js
@@ -0,0 +1,9 @@
+//to run fs - type cls
+
+const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'My name is Misya.')
+
+//challenge: append message to notes.txt
+
+fs.appendFileSync('notes.txt', ' I`m 28 y/o')
