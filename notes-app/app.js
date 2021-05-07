abd3d19113872226197fb2435aa5d0264d78be02
Misyani-DS <54836758+Misyani-DS@users.noreply.github.com>
2021-05-07 12:05:21 +0800

    import files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,30 @@
+//section 3
+
+//Importing Node.js Core Modules
+
 //to run fs - type cls
 
-const fs = require('fs')
+// const fs = require('fs')
 
 // fs.writeFileSync('notes.txt', 'My name is Misya.')
 
 //challenge: append message to notes.txt
 
-fs.appendFileSync('notes.txt', ' I`m 28 y/o')
+// fs.appendFileSync('notes.txt', ' I`m 28 y/o')
+
+//Importing your own files
+
+//import string
+// const firstName = require('./utils.js')
+// console.log(firstName)
+
+//import function
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
+
+//challenge
+
+const notes = require('./notes.js')
+const msg = notes()
+console.log(msg)
---
+++ notes-app/utils.js
@@ -0,0 +1,11 @@
+console.log('utils.js')
+
+//string
+const name = 'Misya'
+// module.exports = name
+
+//function
+const add = function (a, b) {
+  return a + b
+}
+module.exports = add
