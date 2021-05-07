6dad6e243fcff88c3239e8d757131dbdeba41d03
Misyani-DS <54836758+Misyani-DS@users.noreply.github.com>
2021-05-07 14:41:58 +0800

    color printing

--- notes-app/app.js
+++ notes-app/app.js
@@ -25,6 +25,35 @@
 
 //challenge
 
-const notes = require('./notes.js')
-const msg = notes()
-console.log(msg)
+// const notes = require('./notes.js')
+// const msg = notes()
+// console.log(msg)
+
+//Import npm modules
+
+//npm init - create package.json
+//npm install validator@13.6.0 - create node_modules and package-lock.json
+
+// const validator = require('validator')
+
+// console.log(validator.isEmail('misya@test.com')) //return true
+// console.log(validator.isEmail('misyatest.com')) //return false
+// console.log(validator.isURL('google.com')) //return false
+// console.log(validator.isURL('misyatest.com')) //return false
+
+//Printing in color
+
+//challenge
+
+const chalk = require('chalk')
+
+const greenMsg = chalk.green('Success!')
+console.log(greenMsg)
+const boldMsg = chalk.bold.red('Error!')
+console.log(boldMsg)
+const bgMsg = chalk.underline.bgBlue('Hello!')
+console.log(bgMsg)
+const inverseMsg = chalk.inverse.blue('Hello!')
+console.log(inverseMsg)
+
+//Global npm modules and nodemon
