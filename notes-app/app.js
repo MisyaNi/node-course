cd4ac5e4d0c21460cbe8f27a889c55c5d0497ae8
Misyani-DS <54836758+Misyani-DS@users.noreply.github.com>
2021-05-10 15:26:03 +0800

    yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -78,7 +78,72 @@
 //run node app.js add 
 
 //2. argument passing with yargs
-
+//pass into obj
 const yargs = require('yargs')
-console.log(process.argv)
-console.log(yargs.argv)
+// console.log(process.argv)
+
+//customize yargs version
+yargs.version('1.1.0')
+
+
+//create add command
+
+yargs.command({
+  command: 'add',
+  describe: 'Add new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note Description',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
+  }
+
+})
+
+//create remove command
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note...')
+  }
+
+})
+
+//create list command
+
+yargs.command({
+  command: 'list',
+  describe: 'List of notes',
+  handler: function () {
+    console.log('Listing out all notes...')
+  }
+
+})
+
+//create read command
+
+yargs.command({
+  command: 'read',
+  describe: 'Reading a note',
+  handler: function () {
+    console.log('Reading a note..')
+  }
+
+})
+
+yargs.parse()
+
+// console.log(yargs.argv)
+
